bf90459c7ee73fa24a0bc0f5785779f5a5f335e5
Aryan <as10071999@gmail.com>
2021-02-15 02:11:31 +0530

    🚧  Made Post, Delete and update

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from .view import ArticleListView, ArticleDetailView
+from .view import (
+    ArticleListView,
+    ArticleDetailView,
+    ArticleCreateView,
+    ArticleDeleteView,
+    ArticleUpdateView,
+)
 
 urlpatterns = [
     path('', ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view())
+    path('create/',ArticleCreateView.as_view()),
+    path('<pk>', ArticleDetailView.as_view()),
+    path('<pk>/update/', ArticleUpdateView.as_view()),
+    path('<pk>/delete/', ArticleDeleteView.as_view()),
 ]
